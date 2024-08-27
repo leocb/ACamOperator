c893af0bacdf481b6b66fba30e49b611ec553c8b
Leo Bottaro <github@leobottaro.com>
2024-08-26 22:10:35 -0300

    Tidy up the code

--- src/main.rs
+++ src/main.rs
@@ -1,103 +1,50 @@
-use std::path::Path;
 use anyhow::{Error, Result}; // Automatically handle the error types
 use opencv::{
     prelude::*,
-    objdetect,
     imgproc,
     core,
     videoio,
     highgui,
 };
-use opencv::core::{float16_t, MatIter, Rect, Scalar, Size, TickMeter, Vector};
+use opencv::core::{Scalar, Size, TickMeter, Vector};
 use opencv::objdetect::FaceDetectorYN;
-use opencv::sys::{cv_FaceDetectorYN_create_const_StringR_const_StringR_const_SizeR, cv_FaceDetectorYN_create_const_StringR_const_StringR_const_SizeR_float_float_int_int_int};
 use opencv::videoio::{CAP_PROP_FRAME_HEIGHT, CAP_PROP_FRAME_WIDTH};
 
 
-fn visualize(input: &mut Mat, frame: i32, faces: &Mat, fps: f64, thickness: i32) -> anyhow::Result<()> {
+fn visualize(input: &mut Mat, faces: &Mat, fps: f64, thickness: i32) -> Result<()> {
     let fps_string = format!("FPS : {:.2}", fps);
-    if frame >= 0 {
-        println!("Frame {}, ", frame);
-    }
-    println!("FPS: {}", fps_string);
     for i in 0..faces.rows() {
-        // Print results
-        println!(
-            "Face {i}, top-left coordinates: ({}, {}), box width: {}, box height: {}, score: {:.2}",
-            faces.at_2d::<f32>(i, 0)?,
-            faces.at_2d::<f32>(i, 1)?,
-            faces.at_2d::<f32>(i, 2)?,
-            faces.at_2d::<f32>(i, 3)?,
-            faces.at_2d::<f32>(i, 14)?
-        );
-
         // Draw bounding box
-        let rect = core::Rect2f::new(
-            *faces.at_2d::<f32>(i, 0)?,
-            *faces.at_2d::<f32>(i, 1)?,
-            *faces.at_2d::<f32>(i, 2)?,
-            *faces.at_2d::<f32>(i, 3)?,
-        )
-            .to::<i32>()
-            .ok_or_else(|| Error::msg("Invalid rect"))?;
+        let x = *faces.at_2d::<f32>(i, 0)?;
+        let y = *faces.at_2d::<f32>(i, 1)?;
+        let w = *faces.at_2d::<f32>(i, 2)?;
+        let h = *faces.at_2d::<f32>(i, 3)?;
+        let score = *faces.at_2d::<f32>(i, 14)?;
+        let rect = core::Rect2f::new(x,y,w,h).to::<i32>().unwrap();
         imgproc::rectangle(input, rect, (0., 255., 0.).into(), thickness, imgproc::LINE_8, 0)?;
-        // Draw landmarks
-        imgproc::circle(
+
+        // info text
+        imgproc::put_text(
             input,
-            core::Point2f::new(*faces.at_2d::<f32>(i, 4)?, *faces.at_2d::<f32>(i, 5)?)
-                .to::<i32>()
-                .ok_or_else(|| Error::msg("Invalid point"))?,
-            2,
-            (255., 0., 0.).into(),
-            thickness,
-            imgproc::LINE_8,
-            0,
-        )?;
-        imgproc::circle(
-            input,
-            core::Point2f::new(*faces.at_2d::<f32>(i, 6)?, *faces.at_2d::<f32>(i, 7)?)
-                .to::<i32>()
-                .ok_or_else(|| Error::msg("Invalid point"))?,
-            2,
-            (0., 0., 255.).into(),
-            thickness,
-            imgproc::LINE_8,
-            0,
-        )?;
-        imgproc::circle(
-            input,
-            core::Point2f::new(*faces.at_2d::<f32>(i, 8)?, *faces.at_2d::<f32>(i, 9)?)
-                .to::<i32>()
-                .ok_or_else(|| Error::msg("Invalid point"))?,
-            2,
+            format!("score: {score:.2}").as_str(),
+            core::Point::new(x as i32, y as i32 - 5),
+            imgproc::FONT_HERSHEY_SIMPLEX,
+            0.5,
             (0., 255., 0.).into(),
             thickness,
             imgproc::LINE_8,
-            0,
+            false,
         )?;
-        imgproc::circle(
-            input,
-            core::Point2f::new(*faces.at_2d::<f32>(i, 10)?, *faces.at_2d::<f32>(i, 11)?)
-                .to::<i32>()
-                .ok_or_else(|| Error::msg("Invalid point"))?,
-            2,
-            (255., 0., 255.).into(),
-            thickness,
-            imgproc::LINE_8,
-            0,
-        )?;
-        imgproc::circle(
-            input,
-            core::Point2f::new(*faces.at_2d::<f32>(i, 12)?, *faces.at_2d::<f32>(i, 13)?)
-                .to::<i32>()
-                .ok_or_else(|| Error::msg("Invalid point"))?,
-            2,
-            (0., 255., 255.).into(),
-            thickness,
-            imgproc::LINE_8,
-            0,
-        )?;
+
+        // Draw landmarks
+        visualize_draw_point(input, faces, thickness, i, (255., 0., 0.).into(), 4)?;
+        visualize_draw_point(input, faces, thickness, i, (0., 0., 255.).into(), 6)?;
+        visualize_draw_point(input, faces, thickness, i, (0., 255., 0.).into(), 8)?;
+        visualize_draw_point(input, faces, thickness, i, (255., 0., 255.).into(), 10)?;
+        visualize_draw_point(input, faces, thickness, i, (0., 255., 255.).into(), 12)?;
     }
+
+    // FPS
     imgproc::put_text(
         input,
         &fps_string,
@@ -112,17 +59,33 @@ fn visualize(input: &mut Mat, frame: i32, faces: &Mat, fps: f64, thickness: i32)
     Ok(())
 }
 
+fn visualize_draw_point(input: &mut Mat, faces: &Mat, thickness: i32, i: i32, color: Scalar, landmark_offset: i32) -> Result<(), Error> {
+    imgproc::circle(
+        input,
+        core::Point2f::new(
+            *faces.at_2d::<f32>(i, landmark_offset)?,
+            *faces.at_2d::<f32>(i, landmark_offset + 1)?
+        ).to::<i32>().unwrap(),
+        2,
+        color,
+        thickness,
+        imgproc::LINE_8,
+        0,
+    )?;
+    Ok(())
+}
+
 // Note, the namespace of OpenCV is changed (to better or worse). It is no longer one enormous.
 fn main() -> Result<()> { // Note, this is anyhow::Result
 
     // Webcam/Input resolution
-    let width = 1280.;
-    let height = 720.;
+    let width = 1920.;
+    let height = 1080.;
 
     // Open a GUI window
     highgui::named_window("ACamOperator", highgui::WINDOW_NORMAL)?;
     // Open the web-camera (assuming you have one)
-    let mut cam = videoio::VideoCapture::new(1, videoio::CAP_DSHOW)?;
+    let mut cam = videoio::VideoCapture::new(2, videoio::CAP_ANY)?;
     cam.set(CAP_PROP_FRAME_WIDTH, width)?;
     cam.set(CAP_PROP_FRAME_HEIGHT, height)?;
 
@@ -130,9 +93,10 @@ fn main() -> Result<()> { // Note, this is anyhow::Result
     let mut tm = TickMeter::default()?;
 
     // Create the Yunet face detection and load NN weights
-    let yunet_weights_path = std::env::current_dir?.join("models").join("yunet.onnx").into_os_string().into_string()?;
+    let current_dir = std::env::current_dir().unwrap();
+    let yunet_weights_path = current_dir.join("models").join("yunet.onnx").into_os_string().into_string().unwrap();
     let mut face_detector = FaceDetectorYN::create(
-        yunet_weights_path,
+        &yunet_weights_path,
         "",
         Size::new(width as i32, height as i32),
         0.8f32,
@@ -142,16 +106,6 @@ fn main() -> Result<()> { // Note, this is anyhow::Result
         0)?;
 
 
-
-    // Visualization colors
-    let box_color: Scalar = Scalar::new(0f64, 255f64, 0f64, 255f64);
-    let mut landmark_color:Vector<Scalar> = Vector::new();
-    landmark_color.push(Scalar::new(255f64,   0f64,   0f64, 255f64)); // right eye
-    landmark_color.push(Scalar::new(  0f64,   0f64, 255f64, 255f64)); // left eye
-    landmark_color.push(Scalar::new(  0f64, 255f64,   0f64, 255f64)); // nose tip
-    landmark_color.push(Scalar::new(255f64,   0f64, 255f64, 255f64)); // right mouth corner
-    landmark_color.push(Scalar::new(  0f64, 255f64, 255f64, 255f64)); // left mouth corner
-    let text_color: Scalar =  Scalar::new(0f64, 255f64, 0f64, 255f64);
 
     loop {
         tm.start()?;
@@ -165,33 +119,13 @@ fn main() -> Result<()> { // Note, this is anyhow::Result
         let mut faces = Mat::default();
         face_detector.detect(&cam_raw,&mut faces)?;
 
-        let output_image = cam_raw.clone();
+        let mut output_image = cam_raw.clone();
         tm.stop()?;
 
-        // if (fps >= 0)
-        // {
-        //     cv::putText(output_image, cv::format("FPS: %.2f", fps), cv::Point(0, 15), cv::FONT_HERSHEY_SIMPLEX, 0.5, text_color, 2);
-        // }
-
-        for i in 0..faces.rows()
-        {
-            visualize(&mut cam_raw, -1, &faces, tm.get_fps()?, 2)?;
-
-            //
-            // // Confidence as text
-            // float conf = faces.at<float>(i, 14);
-            // cv::putText(output_image, cv::format("%.4f", conf), cv::Point(x1, y1+12), cv::FONT_HERSHEY_DUPLEX, 0.5, text_color);
-            //
-            // // Draw landmarks
-            // for (int j = 0; j < landmark_color.size(); ++j)
-            // {
-            //     int x = static_cast<int>(faces.at<float>(i, 2*j+4)), y = static_cast<int>(faces.at<float>(i, 2*j+5));
-            //     cv::circle(output_image, cv::Point(x, y), 2, landmark_color[j], 2);
-            // }
-        }
+        visualize(&mut output_image, &faces, tm.get_fps()?, 2)?;
 
         // and display in the window
-        highgui::imshow("ACamOperator", &cam_raw)?;
+        highgui::imshow("ACamOperator", &output_image)?;
         let key = highgui::wait_key(1)?;
         if key == 113 { // quit with q
             break;
